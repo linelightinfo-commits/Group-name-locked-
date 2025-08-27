e6f74d492be4874c0d3e0d9fbe8f324d58d4472d
linelightinfo-commits <linelightinfo@gmail.com>
2025-08-27 23:16:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const appState = JSON.parse(fs.readFileSync("appstate.json", "utf-8"));
 
 // Apna Group ID aur Name yahan daal
-const GROUP_THREAD_ID = "7856010577456992";
+const GROUP_THREAD_ID = "785601057456992";
 const LOCKED_GROUP_NAME = "FUCKER HERE"; 
 
 const app = express();
@@ -77,6 +77,7 @@ login({ appState }, (err, api) => {
   startBot(api);
 
 });
+
 
 
 
